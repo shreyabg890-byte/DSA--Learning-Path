dcddad65da84d2c4228494ff3cf1da3f7efe6a37
SHREYA B G <shreyabg890@gmail.com>
2026-03-25 19:59:02 +0530

    Time: 33 ms (32.90%) | Memory: 156.1 MB (13.35%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0 || k == 0) return new int[0];
+
+        int[] result = new int[n - k + 1];
+        Deque<Integer> dq = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // Remove elements out of window
+            if (!dq.isEmpty() && dq.peekFirst() == i - k) {
+                dq.pollFirst();
+            }
+
+            // Remove smaller elements from back
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+
+            // Add current index
+            dq.offerLast(i);
+
+            // Store result when window is complete
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[dq.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+}
